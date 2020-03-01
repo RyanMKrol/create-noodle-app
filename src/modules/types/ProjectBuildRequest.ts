15217fc5d6916b46074bb7e8baa3d5352709e1b3
Ryan Krol <ryankr@hotmail.co.uk>
2020-03-01 17:04:15 +0000

    Add types module with ProjectBuildRequest

---
+++ src/modules/types/ProjectBuildRequest.ts
@@ -0,0 +1,6 @@
+export interface ProjectBuildRequest {
+  projectName: string
+  projectPath: string
+  sourcePath: string
+  targetPath: string
+}
---
+++ src/modules/types/index.ts
@@ -0,0 +1,1 @@
+export { ProjectBuildRequest } from './ProjectBuildRequest'
