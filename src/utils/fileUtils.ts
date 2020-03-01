6187ded3ec1d3295ab280905bddf6f2818ddac2e
Ryan Krol <ryankr@hotmail.co.uk>
2020-03-01 15:59:18 +0000

    Add createDirectoryContents to fileUtils

--- src/utils/fileUtils.ts
+++ src/utils/fileUtils.ts
@@ -1,5 +1,14 @@
 import * as fs from 'fs'
+import * as path from 'path'
 import chalk from 'chalk'
+
+import {
+  replaceTemplateVariables,
+  currentDirectory,
+  TemplateConfig
+} from './'
+
+const SKIP_FILES = ['node_modules', '.template.json']
 
 function createProject(projectPath: string) {
   if (fs.existsSync(projectPath)) {
@@ -12,6 +21,35 @@ function createProject(projectPath: string) {
   return true
 }
 
+function createDirectoryContents(templatePath: string, projectName: string, repoName: string, config: TemplateConfig) {
+  const filesToCreate = fs.readdirSync(templatePath)
+  const dir: string = currentDirectory()
+
+  filesToCreate.forEach(file => {
+    const origFilePath = path.join(templatePath, file)
+
+    // get stats about the current file
+    const stats = fs.statSync(origFilePath)
+
+    if (SKIP_FILES.indexOf(file) > -1) return
+
+    if (stats.isFile()) {
+      let contents = fs.readFileSync(origFilePath, 'utf8')
+
+      contents = replaceTemplateVariables(contents, { projectName, repoName })
+
+      const writePath = path.join(dir, projectName, file)
+      fs.writeFileSync(writePath, contents, 'utf8')
+    } else if (stats.isDirectory()) {
+      fs.mkdirSync(path.join(dir, projectName, file))
+
+      // recursive call
+      createDirectoryContents(path.join(templatePath, file), path.join(projectName, file), repoName, config)
+    }
+  })
+}
+
 export {
-  createProject
+  createProject,
+  createDirectoryContents,
 }
