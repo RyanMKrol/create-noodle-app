4818cb7b36d8fa08794ca477d6f2f7221a55a68e
Ryan Krol <ryankr@hotmail.co.uk>
2020-03-01 17:23:11 +0000

    Add messages module

---
+++ src/modules/messages/index.ts
@@ -0,0 +1,1 @@
+export { messageFilesCreated } from './messageFilesCreated'
---
+++ src/modules/messages/messageFilesCreated.ts
@@ -0,0 +1,6 @@
+import chalk from 'chalk'
+
+export function messageFilesCreated() {
+  console.log('')
+  console.log(chalk.green(`Created your project files!`))
+}
