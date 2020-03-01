14f21b815634104800359d7b432cbf2f0d5d30fa
Ryan Krol <ryankr@hotmail.co.uk>
2020-03-01 15:59:42 +0000

    Update main file to use utils modules

--- src/index.ts
+++ src/index.ts
@@ -4,100 +4,54 @@ import * as inquirer from 'inquirer'
 import * as fs from 'fs'
 import * as path from 'path'
 import * as shell from 'shelljs'
-import * as template from './utils/template'
 import chalk from 'chalk'
 import * as yargs from 'yargs'
 
-const CHOICES = fs.readdirSync(path.join(__dirname, 'templates'))
-
-const QUESTIONS = [
-  {
-    name: 'template',
-    type: 'list',
-    message: 'What project template would you like to generate?',
-    choices: CHOICES,
-    when: () => !yargs.argv['template']
-  },
-  {
-    name: 'name',
-    type: 'input',
-    message: 'Project name (required):',
-    when: () => !yargs.argv['name'],
-    validate: (input: string) => {
-      if (input.length === 0) {
-        return 'Must provide a project name!'
-      }
-
-      if (!(/^([A-Za-z\-\_\d])+$/.test(input))) {
-        return 'Project name may only include letters, numbers, underscores and hashes.'
-      }
-
-      return true
-    }
-  }
-]
-
-const CURR_DIR = process.cwd()
-
-export interface TemplateConfig {
-  files?: string[]
-  postMessage?: string
-}
-
-export interface CliOptions {
-  projectName: string
-  templateName: string
-  templatePath: string
-  tartgetPath: string
-  config: TemplateConfig
-}
+import {
+  QUESTIONS,
+  createProject,
+  createDirectoryContents,
+  currentDirectory,
+  messageDone,
+  TemplateConfig,
+  CliOptions,
+} from './utils'
 
 inquirer.prompt(QUESTIONS)
   .then(answers => {
 
-    answers = Object.assign({}, answers, yargs.argv)
+    // answers can be passed in on the command line, so we combine them here
+    const completeAnswers: object = Object.assign({}, answers, yargs.argv)
 
-    const projectChoice = answers['template']
-    const projectName = answers['name']
+    const projectChoice = completeAnswers['template']
+    const projectName = completeAnswers['name']
+    const repoName = completeAnswers['repo']
+
     const templatePath = path.join(__dirname, 'templates', projectChoice)
-    const tartgetPath = path.join(CURR_DIR, projectName)
+    const targetPath = path.join(currentDirectory(), projectName)
     const templateConfig = getTemplateConfig(templatePath)
 
     const options: CliOptions = {
       projectName,
+      repoName,
       templateName: projectChoice,
       templatePath,
-      tartgetPath,
+      targetPath,
       config: templateConfig
     }
 
-    if (!createProject(tartgetPath)) {
+    if (!createProject(targetPath)) {
       return
     }
 
-    createDirectoryContents(templatePath, projectName, templateConfig)
+    createDirectoryContents(templatePath, projectName, repoName, templateConfig)
 
     if (!postProcess(options)) {
       return
     }
 
-    showMessage(options)
+    messageDone(options)
   })
-
-function showMessage(options: CliOptions) {
-  console.log('')
-  console.log(chalk.green('Done.'))
-  console.log(chalk.green(`Go into the project: cd ${options.projectName}`))
-
-  const message = options.config.postMessage
-
-  if (message) {
-    console.log('')
-    console.log(chalk.yellow(message))
-    console.log('')
-  }
-
-}
 
 function getTemplateConfig(templatePath: string): TemplateConfig {
   const configPath = path.join(templatePath, '.template.json')
@@ -113,16 +67,6 @@ function getTemplateConfig(templatePath: string): TemplateConfig {
   return {}
 }
 
-function createProject(projectPath: string) {
-  if (fs.existsSync(projectPath)) {
-    console.log(chalk.red(`Folder ${projectPath} exists. Delete or use another name.`))
-    return false
-  }
-
-  fs.mkdirSync(projectPath)
-  return true
-}
-
 function postProcess(options: CliOptions) {
   if (isNode(options)) {
     return postProcessNode(options)
@@ -135,7 +79,7 @@ function isNode(options: CliOptions) {
 }
 
 function postProcessNode(options: CliOptions) {
-  shell.cd(options.tartgetPath)
+  shell.cd(options.targetPath)
 
   let cmd = ''
 
@@ -156,33 +100,4 @@ function postProcessNode(options: CliOptions) {
   }
 
   return true
-}
-
-const SKIP_FILES = ['node_modules', '.template.json']
-
-function createDirectoryContents(templatePath: string, projectName: string, config: TemplateConfig) {
-  const filesToCreate = fs.readdirSync(templatePath)
-
-  filesToCreate.forEach(file => {
-    const origFilePath = path.join(templatePath, file)
-
-    // get stats about the current file
-    const stats = fs.statSync(origFilePath)
-
-    if (SKIP_FILES.indexOf(file) > -1) return
-
-    if (stats.isFile()) {
-      let contents = fs.readFileSync(origFilePath, 'utf8')
-
-      contents = template.render(contents, { projectName })
-
-      const writePath = path.join(CURR_DIR, projectName, file)
-      fs.writeFileSync(writePath, contents, 'utf8')
-    } else if (stats.isDirectory()) {
-      fs.mkdirSync(path.join(CURR_DIR, projectName, file))
-
-      // recursive call
-      createDirectoryContents(path.join(templatePath, file), path.join(projectName, file), config)
-    }
-  })
 }
