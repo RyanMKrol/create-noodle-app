e6b3e4500e572755cf42688249dff38a2aa5b497
Ryan Krol <ryankr@hotmail.co.uk>
2020-03-08 14:02:26 +0000

    Update template to setup an express server

---
+++ templates/api-app/src/app.ts
@@ -0,0 +1,36 @@
+// app.js
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+import createError from 'http-errors'
+import cors from 'cors'
+
+import exampleRouter from './routes/example'
+
+const app = express()
+
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.use('/example/endpoint', exampleRouter)
+
+// catch 404 and forward to error handler
+app.use(function(req: any, res: any, next: any) {
+  next(createError(404))
+})
+
+// error handler
+app.use(function(err: any, req: any, res: any, next: any) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.send('500 - Internal Server Error')
+})
+
+export default app
