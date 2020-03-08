5b7204589e6d114dbf62166e27f4121b8a45179d
Ryan Krol <ryankr@hotmail.co.uk>
2020-03-08 15:38:35 +0000

    Add step in file creation to convert package.txt to package.json
    * when publishing this tool, NPM pays attention to every package.json file,
      so when I have a template that's intended for publishing new NPM files, that
      template package.json file messes up the deployment of this tool. To get around
      that, I've stored the template's package.json as package.txt, and I convert it
      here

--- src/modules/files/createProjectFiles.ts
+++ src/modules/files/createProjectFiles.ts
@@ -27,8 +27,10 @@ function createDirectoryContents(sourcePath: string, projectName: string, templa
   const filesToCreate = fs.readdirSync(sourcePath)
   const dir: string = currentDirectory()
 
-  filesToCreate.forEach(file => {
-    const sourceFilePath = path.join(sourcePath, file)
+  filesToCreate.forEach(sourceFileName => {
+    const file = convertSpecialFilenames(sourceFileName)
+
+    const sourceFilePath = path.join(sourcePath, sourceFileName)
     const targetFilePath = path.join(dir, projectName, file)
 
     // get stats about the current file
@@ -46,4 +48,17 @@ function createDirectoryContents(sourcePath: string, projectName: string, templa
       createDirectoryContents(path.join(sourcePath, file), path.join(projectName, file), templateData)
     }
   })
+}
+
+function convertSpecialFilenames(filename: string): string {
+  // when publishing this tool, NPM pays attention to every package.json file,
+  // so when I have a template that's intended for publishing new NPM files, that
+  // template package.json file messes up the deployment of this tool. To get around
+  // that, I've stored the template's package.json as package.txt, and I convert it
+  // here
+  if (filename === 'package.txt') {
+    return 'package.json'
+  }
+
+  return filename
 }
