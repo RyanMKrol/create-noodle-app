c50e977bfb27b537cf41a323fb764c9776ce57cb
Ryan Krol <ryankr@hotmail.co.uk>
2020-09-22 23:06:11 +0100

    Replicate user permissions in project files

--- src/modules/files/createProjectFiles.ts
+++ src/modules/files/createProjectFiles.ts
@@ -6,9 +6,16 @@ import { ProjectBuildRequest, TemplateData } from './../types'
 import { replaceTemplateVariables } from './../template'
 import { messageFilesCreated } from './../messages'
 
+const DEFAULT_GROUP_PERMISSIONS = '5'
+const DEFAULT_OTHER_PERMISSIONS = '5'
+
 export function createProjectFiles(request: ProjectBuildRequest) {
   createDirectory(request.targetPath)
-  createDirectoryContents(request.sourcePath, request.projectName, request.templateData)
+  createDirectoryContents(
+    request.sourcePath,
+    request.projectName,
+    request.templateData
+  )
 
   messageFilesCreated()
 
@@ -23,7 +30,11 @@ function createDirectory(targetPath: string) {
   fs.mkdirSync(targetPath)
 }
 
-function createDirectoryContents(sourcePath: string, projectName: string, templateData: TemplateData) {
+function createDirectoryContents(
+  sourcePath: string,
+  projectName: string,
+  templateData: TemplateData
+) {
   const filesToCreate = fs.readdirSync(sourcePath)
   const dir: string = currentDirectory()
 
@@ -38,14 +49,58 @@ function createDirectoryContents(sourcePath: string, projectName: string, templa
 
     if (stats.isFile()) {
       const rawSourceContent = fs.readFileSync(sourceFilePath, 'utf8')
-      const targetContents = replaceTemplateVariables(rawSourceContent, templateData)
+      const targetContents = replaceTemplateVariables(
+        rawSourceContent,
+        templateData
+      )
+
+      let isReadable = 1
+      let isWriteable = 1
+      let isExecutable = 1
+
+      // Check if the file is readable.
+      fs.access(sourceFilePath, fs.constants.R_OK, err => {
+        isReadable = 0
+      })
+
+      // Check if the file is writable.
+      fs.access(sourceFilePath, fs.constants.W_OK, err => {
+        isWriteable = 0
+      })
+
+      // Check if the file is executable.
+      fs.access(sourceFilePath, fs.constants.X_OK, err => {
+        isExecutable = 0
+      })
+
+      const binaryUserPermissionsValue = `${isReadable}${isWriteable}${isExecutable}`
+      const decimalUserPermissionsValue = Number.parseInt(
+        binaryUserPermissionsValue,
+        2
+      )
 
       fs.writeFileSync(targetFilePath, targetContents, 'utf8')
+
+      fs.chmod(
+        targetFilePath,
+        `${decimalUserPermissionsValue}${DEFAULT_GROUP_PERMISSIONS}${DEFAULT_OTHER_PERMISSIONS}`,
+        error => {
+          if (error) {
+            console.log(
+              `Unable to change permissions of file:${targetFilePath}, due to error: ${error}`
+            )
+          }
+        }
+      )
     } else if (stats.isDirectory()) {
       fs.mkdirSync(path.join(dir, projectName, file))
 
       // recursive call
-      createDirectoryContents(path.join(sourcePath, file), path.join(projectName, file), templateData)
+      createDirectoryContents(
+        path.join(sourcePath, file),
+        path.join(projectName, file),
+        templateData
+      )
     }
   })
 }
